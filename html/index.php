2345740ac1d7facfd9ca9c96f1503895ff447d32
Emanuel Trigo <emantrigo@hotmail.com>
2021-03-18 17:52:40 +0000

    fixed some footers

--- html/index.php
+++ html/index.php
@@ -15,16 +15,13 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
+        <div class="collapse navbar-collapse h-50" id="navbarScroll">
           <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="./backoffice">Settings</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="loginreg.html">Login | Register</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="viewcart.html">Shopping Cart</a>
