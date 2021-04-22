417385d391de3fca830b9db708c42b8d514f1c37
Emanuel Trigo <emantrigo@hotmail.com>
2021-04-22 12:00:02 +0100

    added database scripts

--- database/create.sql
+++ database/create.sql
@@ -1,34 +1,40 @@
-DROP TABLE IF EXISTS answer;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS review;
-DROP TABLE IF EXISTS orderTotal;
-DROP TABLE IF EXISTS orderedProduct;
-DROP TABLE IF EXISTS buyer;
-DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS paymentMethod;
+DROP TABLE IF EXISTS orderTotal;
+DROP TABLE IF EXISTS orderedProduct;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS answer;
 DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS cupon;
+DROP TABLE IF EXISTS whishlist;
+DROP TABLE IF EXISTS promotion;
+DROP TABLE IF EXISTS cartProduct;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS buyer;
+DROP TABLE IF EXISTS category;
 
-DROP TYPE IF EXISTS rate;
-
--- Types
  
-CREATE TYPE rate AS ENUM ('poor','bad', 'neutral', 'good', 'excellent');
-
 -- Tables
+
+CREATE TABLE category(
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE NOT NULL	
+	
+);
 
 CREATE TABLE buyer (
     id SERIAL PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL CONSTRAINT user_email_uk UNIQUE,
-    phoneNumber INTEGER NOT NULL
+    phoneNumber text NOT NULL
 );
  
 CREATE TABLE product (
     id SERIAL PRIMARY KEY,
     name text NOT NULL,
     price INTEGER NOT NULL,
-    category text NOT NULL,
+    category INTEGER REFERENCES category (id),
     description text NOT NULL,
     quantity INTEGER NOT NULL
 );
@@ -58,10 +64,10 @@ CREATE TABLE orderTotal (
 );
  
 CREATE TABLE orderedProduct (
+    id SERIAL PRIMARY KEY,
     id_buyer INTEGER NOT NULL REFERENCES buyer (id) ON UPDATE CASCADE,
     id_product INTEGER NOT NULL REFERENCES product (id) ON UPDATE CASCADE,
-    PRIMARY KEY (id_buyer, id_product),
-    quantity INTEGER NOT NULL CONSTRAINT order_ck CHECK (quantity > 0)
+    quantity INTEGER NOT NULL
 );
  
 CREATE TABLE review (
@@ -70,7 +76,7 @@ CREATE TABLE review (
     PRIMARY KEY (id_buyer, id_product),
     reviewText text NOT NULL,
     date TIMESTAMP WITH TIME zone NOT NULL,
-    rating rate NOT NULL  
+    rating INTEGER NOT NULL CONSTRAINT rating_ck CHECK (((rating > 0) OR (rating <= 5)))
 
 );
  
@@ -83,12 +89,13 @@ CREATE TABLE question (
 );
  
 
+ 
 CREATE TABLE admin (
-    id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL CONSTRAINT admin_email_uk UNIQUE,
     password text NOT NULL,
-    phoneNumber INTEGER NOT NULL,
+    phoneNumber text NOT NULL,
     super BOOLEAN DEFAULT False NOT NULL
 );
 
@@ -98,4 +105,35 @@ CREATE TABLE answer (
     date TIMESTAMP WITH TIME zone NOT NULL,
     id_question INTEGER NOT NULL REFERENCES question (id) ON UPDATE CASCADE,
     id_admim INTEGER NOT NULL REFERENCES admin (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE cupon (
+	id SERIAl PRIMARY KEY,
+	name text NOT NULL,
+	discount INTEGER NOT NULL CONSTRAINT discoun_ck CHECK (discount > 0),
+	validUntil TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE cartProduct(
+	idBuyer INTEGER NOT NULL REFERENCES buyer (id),
+	idProduct INTEGER NOT NULL REFERENCES product (id),
+	PRIMARY KEY (idBuyer,idProduct),	
+	quantity INTEGER NOT NULL CONSTRAINT quantity_ck CHECK (quantity > 0)
+	
+);
+
+CREATE TABLE whishlist(
+	idBuyer INTEGER NOT NULL REFERENCES buyer (id),
+	idProduct INTEGER NOT NULL REFERENCES product (id),
+	PRIMARY KEY (idBuyer,idProduct)	
+	
+);
+
+
+
+CREATE TABLE promotion(
+	id SERIAL PRIMARY KEY,
+	discount INTEGER NOT NULL CONSTRAINT discount2_ck CHECK (discount > 0),
+	validUNTIL TIMESTAMP NOT NULL DEFAULT NOW()
+	
 );
